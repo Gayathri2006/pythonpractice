b79a46ff61ff6d2f914c7c21ba52211f3450f875
Gayathri <g3.botta@gmail.com>
2021-06-24 21:53:24 +0100

    task 1

--- IdentifyNumber.py
+++ IdentifyNumber.py
@@ -1,19 +1,15 @@
 # Program to input from user and validate
 
-
-
-
 while True:
-    while True:
-        try:
-            num = float(input("Type a number:"))
-        except ValueError:
-            print("This is not a number.")
-            continue
-        else:
+    try: # try is used to catch any errors for the input validation into float
+        num = float(input("Please input a number: "))
+    except ValueError: # ValueError is raised when we are attempting to convert a non numeric string into float.
+        print("This is not a valid number, please try again.")
+        continue
+    else: # should be a number, but check for within range
+        if -9999 < num < 9999:
+            print("Congrats! You have given a valid number!")
             break
-    if -9999 < num < 9999:
-        print("valid")
-        break
-    else:
-     print("Enter a number between -9999 and 9999")
+        else:
+            print("Enter a number between -9999 and 9999")
+            continue
